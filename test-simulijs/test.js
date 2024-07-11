cc1a9ffcf09722c21e956057904ccf0bd551ce2d
hrithikmani <hrithik.mani2000@gmail.com>
2024-07-10 21:12:44 -0400

    Restructurung test cases

--- test-simulijs/test.js
+++ test-simulijs/test.js
@@ -2,27 +2,41 @@ const puppeteer = require('puppeteer');
 
 const delay = (time) => new Promise((resolve) => setTimeout(resolve, time));
 
+// Constants
+const BASE_URL = `file://${__dirname}`;
+const CLICK_TEST_PATH = '/click-test.html';
+const KEYPRESS_TEST_PATH = '/keypress-test.html';
+const DELAY_TIME = 3000;
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   page.on('console', msg => console.log('PAGE LOG:', msg.text()));
 
-  // Test Click Event
-  await page.goto(`file://${__dirname}/click-test.html`);
-  await delay(3000); // Wait for the event to be logged
-  const clickResult = await page.$eval('#clickResult', el => el.textContent);
-  console.log('Click Result:', clickResult);
-  console.log('Click event test completed:', clickResult === 'Button was clicked!');
+  // Function to test click event
+  const testClickEvent = async () => {
+    await page.goto(`${BASE_URL}${CLICK_TEST_PATH}`);
+    await delay(DELAY_TIME);
+    const clickResult = await page.$eval('#clickResult', el => el.textContent);
+    console.log('Click Result:', clickResult);
+    console.log('Click event test completed:', clickResult === 'Button was clicked!');
+  };
 
-  // Test Keypress Event
-  await page.goto(`file://${__dirname}/keypress-test.html`);
-  await delay(3000); // Wait for the event to be logged
-  const keypressResult = await page.$eval('#keypressResult', el => el.textContent);
-  const inputValue = await page.$eval('#testInput', el => el.value);
-  console.log('Keypress Result:', keypressResult);
-  console.log('Input Value:', inputValue);
-  console.log('Keypress event test completed:', keypressResult === 'Key "abcd" was pressed!' && inputValue === 'abcd');
+  // Function to test keypress event
+  const testKeypressEvent = async () => {
+    await page.goto(`${BASE_URL}${KEYPRESS_TEST_PATH}`);
+    await delay(DELAY_TIME);
+    const keypressResult = await page.$eval('#keypressResult', el => el.textContent);
+    const inputValue = await page.$eval('#testInput', el => el.value);
+    console.log('Keypress Result:', keypressResult);
+    console.log('Input Value:', inputValue);
+    console.log('Keypress event test completed:', keypressResult === 'Key "abcd" was pressed!' && inputValue === 'abcd');
+  };
+
+  // Run tests
+  await testClickEvent();
+  await testKeypressEvent();
 
   await browser.close();
 })();
